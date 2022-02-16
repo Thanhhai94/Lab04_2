d98c31df563378f8ac62d768592c05f9fd3ffcf8
Thanhhai94 <thanhhai.utc.k53@gmail.com>
2022-02-16 12:44:45 +0700

    Functional Components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Main from './components/MainComponent'
+import React from 'react';
+
+class App extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <Main />
+      </div>
+      
+    );
+  }
+  
+}
+
+export default App;
